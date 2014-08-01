b772873e1aee29a5c7e048a15ebecab8e04414d5
Néstor Salceda <nestor.salceda@gmail.com>
2014-08-01 19:18:33 +0200

    Send colors when enabling

--- webapp/lib/bolt/services.rb
+++ webapp/lib/bolt/services.rb
@@ -3,18 +3,18 @@ require 'serialport'
 module Bolt
   class LightService
     def initialize
-      device = '/dev/ttyACM1'
+      device = '/dev/ttyACM0'
       bauds = 115200
       data_bits = 8
       stop_bits = 1
       parity = SerialPort::NONE
 
       @lights = SerialPort.new(device, bauds, data_bits, stop_bits, parity)
-      @enabled = true
+      @enabled = false
     end
 
-    def enable
-      @lights.write('enable')
+    def enable(red=255, green=255, blue=255)
+      @lights.write("enable #{red},#{green},#{blue}")
       @enabled = true
     end
 
