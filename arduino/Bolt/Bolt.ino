3e5ef0bb3ae0151c7b62d3afae04afd9e6738722
Néstor Salceda <nestor.salceda@gmail.com>
2014-08-01 19:18:10 +0200

    Add support for choosing colors

--- arduino/Bolt/Bolt.ino
+++ arduino/Bolt/Bolt.ino
@@ -6,13 +6,14 @@ PololuLedStrip<12> ledStrip;
 rgb_color colors[LED_COUNT];
 
 String command = "";
+String read_colors = "";
 boolean enabled = false;
 
 void setup()
 {
-  disable();
   Serial.begin(115200);
   Serial.println("Ready to receive commands.");
+  disable();
 }
 
 void loop()
@@ -22,17 +23,30 @@ void loop()
     command = Serial.readStringUntil('\n');
     command.trim();
 
-    if (command == "enable")
-    {
-      enable();
-    }
-    else if (command == "disable")
+    if (command == "disable")
     {
       disable();
     }
     else if (command == "enabled?")
     {
       Serial.println(enabled);
+    }
+    else if (command.startsWith("enable"))
+    {
+      read_colors = command.substring(7);
+      byte first_comma = read_colors.indexOf(',');
+      byte last_comma = read_colors.lastIndexOf(',');
+
+      if (read_colors and first_comma != -1 and last_comma != first_comma) {
+        byte red = read_colors.substring(0, first_comma).toInt();
+        byte green = read_colors.substring(first_comma + 1, last_comma).toInt();
+        byte blue = read_colors.substring(last_comma + 1, read_colors.length()).toInt();
+
+        enable(red, green, blue);
+      }
+      else {
+        enable(255, 255, 255);
+      }
     }
     else
     {
@@ -41,12 +55,12 @@ void loop()
   }
 }
 
-void enable()
+void enable(byte red, byte green, byte blue)
 {
   rgb_color color;
-  color.red = 255;
-  color.green = 255;
-  color.blue = 255;
+  color.red = red;
+  color.green = green;
+  color.blue = blue;
   setColor(color);
 
   enabled = true;
