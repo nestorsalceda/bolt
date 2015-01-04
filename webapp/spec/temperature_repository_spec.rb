c42afb0e2aa6d81abc649e1e5ce1f887b3a40a69
Néstor Salceda <nestor.salceda@gmail.com>
2015-01-04 21:36:29 +0100

    Added mean, maximum and minimum temperature

--- webapp/spec/temperature_repository_spec.rb
+++ webapp/spec/temperature_repository_spec.rb
@@ -20,6 +20,32 @@ module Bolt
       expect(result[0]).to eq({:temperature => 21.67, :timestamp => 1413307633})
     end
 
+    it 'finds mean temperature for today' do
+      value = 21.03
+      expect(@influxdb_client).to receive(:query).with("select mean(value) from temperatures where time > #{Date.today}").and_return([{"mean" => value}])
+
+      result = @repository.find_today_mean_temperature
+
+      expect(result).to eq(value)
+    end
+
+    it 'finds minimum temperature for today' do
+      value = 21.03
+      expect(@influxdb_client).to receive(:query).with("select min(value) from temperatures where time > #{Date.today}").and_return([{"min" => value}])
+
+      result = @repository.find_today_minimum_temperature
+
+      expect(result).to eq(value)
+    end
+    it 'finds maximum temperature for today' do
+      value = 21.03
+      expect(@influxdb_client).to receive(:query).with("select max(value) from temperatures where time > #{Date.today}").and_return([{"max" => value}])
+
+      result = @repository.find_today_maximum_temperature
+
+      expect(result).to eq(value)
+    end
+
     def stubbed_temperatures
       [{"time" => 1413307633, "value" => 21.67}, {"time" => 1413307693, "value" => 21.56}, {"time" => 1413307753, "value" => 21.56}]
     end
